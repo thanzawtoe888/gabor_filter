0dfbe6f34b2ca4a3c690714e4af803bec7ae5b99
Luca <dev.thanzawtoe@gmail.com>
2025-01-27 14:33:52 +0700

    gabor_dataset_read--SVM-ok---Plt.show not okgit status_

--- code/gabor_dataset_read.py
+++ code/gabor_dataset_read.py
@@ -129,17 +129,17 @@ image_features = np.expand_dims(image_features, axis=0)
 X_for_RF = np.reshape(image_features, (x_train.shape[0], -1))  #Reshape to #images, features
 
 #Define the classifier
-from sklearn.ensemble import RandomForestClassifier
-RF_model = RandomForestClassifier(n_estimators = 50, random_state = 42)
+#from sklearn.ensemble import RandomForestClassifier
+#RF_model = RandomForestClassifier(n_estimators = 50, random_state = 42)
 
 #Can also use SVM but RF is faster and may be more accurate.
-#from sklearn import svm
-#SVM_model = svm.SVC(decision_function_shape='ovo')  #For multiclass classification
-#SVM_model.fit(X_for_RF, y_train)
+from sklearn import svm
+SVM_model = svm.SVC(decision_function_shape='ovo')  #For multiclass classification
+SVM_model.fit(X_for_RF, y_train)
 
 
 # Fit the model on training data
-RF_model.fit(X_for_RF, y_train) #For sklearn no one hot encoding
+#RF_model.fit(X_for_RF, y_train) #For sklearn no one hot encoding
 
 #Predict on Test data
 #Extract features from test data and reshape, just like training data
@@ -148,7 +148,7 @@ test_features = np.expand_dims(test_features, axis=0)
 test_for_RF = np.reshape(test_features, (x_test.shape[0], -1))
 
 #Predict on test
-test_prediction = RF_model.predict(test_for_RF)
+test_prediction = SVM_model.predict(test_for_RF)
 #Inverse le transform to get original label back. 
 test_prediction = le.inverse_transform(test_prediction)
 
@@ -156,9 +156,14 @@ test_prediction = le.inverse_transform(test_prediction)
 from sklearn import metrics
 print ("Accuracy = ", metrics.accuracy_score(test_labels, test_prediction))
 
+#print classification report
+from sklearn.metrics import classification_report
+print(classification_report(test_labels, test_prediction))
+
 #Print confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(test_labels, test_prediction)
+
 
 fig, ax = plt.subplots(figsize=(6,6))         # Sample figsize in inches
 sns.set(font_scale=1.6)
@@ -168,7 +173,7 @@ sns.heatmap(cm, annot=True, ax=ax)
 import random
 n=random.randint(0, x_test.shape[0]-1) #Select the index of image to be loaded for testing
 img = x_test[n]
-plt.imshow(img)
+plt.show(img)
 
 #Extract features and reshape to right dimensions
 input_img = np.expand_dims(img, axis=0) #Expand dims so the input is (num images, x, y, c)
@@ -176,7 +181,7 @@ input_img_features=feature_extractor(input_img)
 input_img_features = np.expand_dims(input_img_features, axis=0)
 input_img_for_RF = np.reshape(input_img_features, (input_img.shape[0], -1))
 #Predict
-img_prediction = RF_model.predict(input_img_for_RF)
+img_prediction = SVM_model.predict(input_img_for_RF)
 img_prediction = le.inverse_transform([img_prediction])  #Reverse the label encoder to original name
 print("The prediction for this image is: ", img_prediction)
 print("The actual label for this image is: ", test_labels[n])
